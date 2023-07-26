1cedf1bd930b6a3638d6b7f669eb98b14c366bdc
Diego <diegoaporterol@gmail.com>
2023-07-25 23:24:35 -0500

    Add frontend

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple transfer file
+
+This is a small app for upload files
+
+[Here](https://github.com/Dandrezz/Simple-tranfer-file-web) you can fint the code of the frontend 
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    file_location = f"{file.filename}"
+    file_location = os.path.join('images',file.filename)
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
     return {"filename": file.filename}
+
+app.mount("/", StaticFiles(directory="frontend", html=True))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
